0b762112c3ed9504390c213ee060d49849775482
Mlie <mlie@litet.net>
2021-12-14 20:52:21 +0100

    Added some extra checks for viewing fish, hopefully fixing some rare error

--- Source/Aquarium/JobDriver_AQViewFishTank.cs
+++ Source/Aquarium/JobDriver_AQViewFishTank.cs
@@ -6,15 +6,20 @@ namespace Aquarium;
 
 public class JobDriver_AQViewFishTank : JobDriver_VisitJoyThing
 {
-    private Thing FishyThing => job.GetTarget(TargetIndex.A).Thing;
+    private Thing FishyThing => job?.GetTarget(TargetIndex.A).Thing;
 
     protected override void WaitTickAction()
     {
+        pawn.GainComfortFromCellIfPossible();
+        if (FishyThing == null)
+        {
+            return;
+        }
+
         var num = AQUtility.GetJoyGainFactor(
             FishyThing.GetStatValue(StatDefOf.Beauty) / FishyThing.def.GetStatValueAbstract(StatDefOf.Beauty),
             FishyThing);
         var extraJoyGainFactor = num > 0f ? num : 0f;
-        pawn.GainComfortFromCellIfPossible();
         JoyUtility.JoyTickCheckEnd(pawn, JoyTickFullJoyAction.EndJob, extraJoyGainFactor, (Building)FishyThing);
         AQUtility.ApplyMoodBoostAndInspire(pawn, FishyThing);
     }
